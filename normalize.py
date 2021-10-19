4f4f8c714240391729cf25d45633070df9239e07
Cole Kettler <me@colekettler.com>
2021-10-19 17:46:56 -0400

    Read CSV file and output contents

--- normalize.py
+++ normalize.py
@@ -1,6 +1,12 @@
-def normalize():
-    return True
+import csv
+import sys
+
+
+def normalize(file):
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
 
 
 if __name__ == "__main__":
-    normalize()
+    normalize(sys.stdin)
