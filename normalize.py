3b42c6174c246f1dc2ebdaf61c0474251cf6ee72
Cole Kettler <me@colekettler.com>
2021-10-19 18:44:24 -0400

    Offset PST timestamp to EST

--- normalize.py
+++ normalize.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 
 
@@ -17,7 +17,12 @@ def normalize_row(row):
 
 def normalize_timestamp(timestamp):
     format = "%m/%d/%y %I:%M:%S %p"
-    return datetime.strptime(timestamp, format).isoformat()
+    dt_pst = datetime.strptime(timestamp, format)
+
+    est_offset = 3
+    dt_est = dt_pst + timedelta(hours=est_offset)
+
+    return dt_est.isoformat()
 
 
 if __name__ == "__main__":
--- test_normalize.py
+++ test_normalize.py
@@ -1,14 +1,14 @@
 import normalize
 
 
-def test_normalize_row_returns_row():
+def test_normalize_row():
     row = "val1,val2"
     assert normalize.normalize_row(row)
 
 
-def test_normalize_timestamp_uses_RFC3339():
+def test_normalize_timestamp():
     timestamp_am = "4/1/11 11:00:00 AM"
-    assert normalize.normalize_timestamp(timestamp_am) == "2011-04-01T11:00:00"
+    assert normalize.normalize_timestamp(timestamp_am) == "2011-04-01T14:00:00"
 
     timestamp_pm = "4/1/11 11:00:00 PM"
-    assert normalize.normalize_timestamp(timestamp_pm) == "2011-04-01T23:00:00"
+    assert normalize.normalize_timestamp(timestamp_pm) == "2011-04-02T02:00:00"
